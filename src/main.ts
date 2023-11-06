475acbf7918327deec5f6a0e7ea759361c84609c
kingmiclong <b302057069@gmail.com>
2023-11-06 11:07:07 -0800

    D3.a collect and deposit

--- src/main.ts
+++ src/main.ts
@@ -4,75 +4,115 @@ import leaflet from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
 
-
 const MERRILL_CLASSROOM = leaflet.latLng({
-    lat: 36.9995,
-    lng: - 122.0533
+  lat: 36.9995,
+  lng: -122.0533,
 });
 
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
-const PIT_SPAWN_PROBABILITY = 0.1;
+const CACHE_SPAWN_PROBABILITY = 0.1;
 
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
 const map = leaflet.map(mapContainer, {
-    center: MERRILL_CLASSROOM,
-    zoom: GAMEPLAY_ZOOM_LEVEL,
-    minZoom: GAMEPLAY_ZOOM_LEVEL,
-    maxZoom: GAMEPLAY_ZOOM_LEVEL,
-    zoomControl: false,
-    scrollWheelZoom: false
+  center: MERRILL_CLASSROOM,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
 });
 
-leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-    attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"
-}).addTo(map);
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
 
-const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
-playerMarker.bindTooltip("That's you!");
-playerMarker.addTo(map);
+// Define the player inventory and cache structure
+interface Cache {
+  location: leaflet.LatLngExpression;
+  marker: leaflet.Marker;
+  coins: number;
+}
 
-let points = 0;
+const player = {
+  location: MERRILL_CLASSROOM,
+  coins: 0, // Player starts with 0 coins
+  marker: leaflet
+    .marker(MERRILL_CLASSROOM)
+    .addTo(map)
+    .bindTooltip("That's you!"),
+};
+
+const caches: Cache[] = [];
+
+// Update the status panel function
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
-statusPanel.innerHTML = "No points yet...";
+const updateStatusPanel = () => {
+  statusPanel.innerHTML = `You have ${player.coins} coins.`;
+};
 
-function makePit(i: number, j: number) {
-    const bounds = leaflet.latLngBounds([
-        [MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + j * TILE_DEGREES],
-        [MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES],
-    ]);
+// Function to create a cache
+function makeCache(i: number, j: number) {
+  const location = leaflet.latLng(
+    MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
+    MERRILL_CLASSROOM.lng + j * TILE_DEGREES
+  );
 
-    const pit = leaflet.rectangle(bounds) as leaflet.Layer;
+  let coins = Math.floor(luck([i, j, "cacheValue"].toString()) * 100);
 
+  const marker = leaflet.marker(location);
+  marker.bindPopup(() => {
+    const container = document.createElement("div");
+    container.innerHTML = `
+      <div>Cache at "${i},${j}" with ${coins} coins.</div>
+      <button id="collect">Collect</button>
+      <button id="deposit">Deposit</button>
+    `;
+    const collectButton =
+      container.querySelector<HTMLButtonElement>("#collect")!;
+    const depositButton =
+      container.querySelector<HTMLButtonElement>("#deposit")!;
 
-
-    pit.bindPopup(() => {
-        let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
-        const container = document.createElement("div");
-        container.innerHTML = `
-                <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
-                <button id="poke">poke</button>`;
-        const poke = container.querySelector<HTMLButtonElement>("#poke")!;
-        poke.addEventListener("click", () => {
-            value--;
-            container.querySelector<HTMLSpanElement>("#value")!.innerHTML = value.toString();
-            points++;
-            statusPanel.innerHTML = `${points} points accumulated`;
-        });
-        return container;
+    // Event listener for collecting coins
+    collectButton.addEventListener("click", () => {
+      player.coins += coins; // Add coins to player's inventory
+      coins = 0; // Cache is now empty
+      updateStatusPanel();
+      marker.getPopup()?.setContent(container.innerHTML); // Update popup content
     });
-    pit.addTo(map);
+
+    // Event listener for depositing coins
+    depositButton.addEventListener("click", () => {
+      if (player.coins > 0) {
+        coins += player.coins; // Add player's coins to cache
+        player.coins = 0; // Player now has 0 coins
+        updateStatusPanel();
+        marker.getPopup()?.setContent(container.innerHTML); // Update popup content
+      }
+    });
+
+    return container;
+  });
+  marker.addTo(map);
+
+  // Add the cache to the caches array
+  caches.push({ location, marker, coins });
 }
 
+// Generate caches
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-    for (let j = - NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-        if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
-            makePit(i, j);
-        }
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      makeCache(i, j);
     }
+  }
 }
+
+// Initial call to update the status panel
+updateStatusPanel();
